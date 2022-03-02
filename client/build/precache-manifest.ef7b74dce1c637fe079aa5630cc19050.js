a6a54be3cfc73fdac8ed18012f8587483363ed19
JoNoC0 <jnguyeningco@gmail.com>
2022-03-02 12:57:56 -0600

    run seed

---
+++ client/build/precache-manifest.ef7b74dce1c637fe079aa5630cc19050.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26dc7d52f69d6256d9f3f537336c97b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31252ff69e42e654d08c",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "bad8166f8b77a451c7ef",
+    "url": "/static/js/2.15806159.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.15806159.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31252ff69e42e654d08c",
+    "url": "/static/js/main.20db8482.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
